bc1bbe1f1bc0475b0f7cb659a2f0ef6fe9328537
Jack Phalen <jbp54799@uga.edu>
2026-09-01 11:25:59 -0400

    fix inputs overflow out of container

--- src/pages/Inputs.jsx
+++ src/pages/Inputs.jsx
@@ -7,11 +7,11 @@ import { UserProvider } from "../context/UserContext";
 
 export default function Inputs() {
   return (
-    <div className="flex items-start justify-center min-h-screen pt-20">
+    <div className="flex items-start justify-center min-h-screen py-6 sm:py-12">
       <UserProvider>
-        <div className="flex items-center justify-center bg-primary-green-500 rounded-md shadow-lg h-[70vh] sm:h-auto sm:aspect-square w-full max-w-none sm:max-w-[70vmin] m-2 ">
-          <div className="flex flex-col shadow-md items-center sm:items-start justify-center bg-[#f9f9f9] rounded-md p-5 size-[90%]">
-            <div className="flex flex-col justify-evenly size-[90%] sm:pl-5">
+        <div className="flex items-center justify-center bg-primary-green-500 rounded-xl shadow-lg min-h-[80vh] md:min-h-[70vh] w-full max-w-none sm:max-w-[70vmin] m-2 p-3 sm:py-10">
+          <div className="flex flex-col shadow-md items-center sm:items-start justify-center bg-[#f9f9f9] rounded-lg md:p-5 p-2 w-[90%]">
+            <div className="flex flex-col justify-evenly w-[90%] sm:pl-5 gap-3">
               <div className="flex flex-row w-full">
                 <h1 className="font-bold text-[36px] md:text-[40px] text-black">
                   Closing Costs Calculator
